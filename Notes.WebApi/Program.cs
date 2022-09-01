07080173fa853b71af4b24548e96a1ba4435498e
alek <user@user>
2022-09-01 22:18:34 +0300

    Add DbContext to startup.cs

--- Notes.WebApi/Program.cs
+++ Notes.WebApi/Program.cs
@@ -1,4 +1,21 @@
+using Notes.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+using(var serviceProvider = services.BuildServiceProvider())
+{
+    try
+    {
+        var context = serviceProvider.GetRequiredService<NotesDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch(Exception ex)
+    {
+
+    }
+}
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
