909a25fa5b9f92ad7692b55bf0625e20a96e2400
alek <user@user>
2022-09-05 22:43:39 +0300

    Create NoteController with CRUD operations. Write some Dto such as CreateNoteDto and UpdateNoteDto

---
+++ Notes.WebApi/Models/UpdateNoteDto.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Notes.UpdateNote;
+
+namespace Notes.WebApi.Models
+{
+    public class UpdateNoteDto: IMapWith<UpdateNoteCommand>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UpdateNoteDto, UpdateNoteCommand>()
+                .ForMember(updateNoteCommand => updateNoteCommand.Id,
+                opt => opt.MapFrom(updateNoteDto => updateNoteDto.Id))
+                .ForMember(updateNoteCommand => updateNoteCommand.Title,
+                opt => opt.MapFrom(updateNoteDto => updateNoteDto.Title))
+                .ForMember(updateNoteCommand => updateNoteCommand.Details,
+                opt => opt.MapFrom(updateNoteDto => updateNoteDto.Details));
+        }
+    }
+}
--- Notes.WebApi/Program.cs
+++ Notes.WebApi/Program.cs
@@ -1,23 +1,56 @@
+using Notes.Application;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Interfaces;
 using Notes.Persistence;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var services = builder.Services;
-using(var serviceProvider = services.BuildServiceProvider())
+var builderServices = builder.Services;
+builderServices.AddApplication();
+IConfiguration configuration = builderServices.BuildServiceProvider().GetRequiredService<IConfiguration>();
+builderServices.AddPersistence(configuration);
+builderServices.AddControllers();
+
+builderServices.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+});
+
+builderServices.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
+var app = builder.Build();
+var services = app.Services;
+
+using (var scope = services.CreateScope())
 {
     try
     {
+        var serviceProvider = scope.ServiceProvider;
         var context = serviceProvider.GetRequiredService<NotesDbContext>();
         DbInitializer.Initialize(context);
     }
-    catch(Exception ex)
+    catch (Exception ex)
     {
 
     }
 }
 
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseEndpoints(endpoints => 
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
