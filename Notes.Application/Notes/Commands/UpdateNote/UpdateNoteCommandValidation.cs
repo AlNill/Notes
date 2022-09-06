22c9778a10ed2de271e5a86bff9d696bd1f8ad7a
alek <user@user>
2022-09-06 20:24:51 +0300

    Add validation rules

---
+++ Notes.Application/Notes/Commands/UpdateNote/UpdateNoteCommandValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Notes.Application.Notes.UpdateNote;
+
+namespace Notes.Application.Notes.Commands.UpdateNote
+{
+    public class UpdateNoteCommandValidation: AbstractValidator<UpdateNoteCommand>
+    {
+        public UpdateNoteCommandValidation()
+        {
+            RuleFor(updateCommand => updateCommand.Title).NotEmpty().MaximumLength(250);
+            RuleFor(updateCommand => updateCommand.UserId).NotEqual(Guid.Empty);
+            RuleFor(updateCommand => updateCommand.Id).NotEqual(Guid.Empty);
+        }
+    }
+}
